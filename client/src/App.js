fd6358534a2b03a4831828e2e5e66ebe3c269366
Sam Park <sampark@v1041-wn-rt-a-159-197.campus-dynamic.uwaterloo.ca>
2024-01-14 13:33:02 -0500

    First Commit

---
+++ client/src/App.js
@@ -0,0 +1,98 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, InputGroup, FormControl, Button, Row, Card } from 'react-bootstrap';
+import { useState, useEffect } from 'react';
+
+const CLIENT_ID = "d94ab512896b4b75a919dc93abf5043d";
+const CLIENT_SECRET = "0035ac1c18b5426da21aad9228132d07";
+
+function App() {
+  const [ searchInput, setSearchInput ] = useState("");
+  const [ accessToken, setAccessToken] = useState("");
+  const [ albums, setAlbums ] = useState([]);
+
+  useEffect(() => {
+    // API Access Token
+    var authParameters = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
+    }
+
+    fetch('https://accounts.spotify.com/api/token', authParameters)
+    .then(result => result.json())
+    .then(data => setAccessToken(data.access_token))
+  }, [])
+
+  // Search
+  async function search() {
+    console.log("Search for " + searchInput); 
+
+    // Get request using search to get the Artist ID
+    var searchParameters = {
+      method: 'Get',
+      headers: {
+        'Content-Type': 'application/json', 
+        'Authorization': 'Bearer ' + accessToken
+      }
+    }
+    var artistID = await fetch('https://api.spotify.com/v1/search?q=' + searchInput + '&type=artist', searchParameters)
+      .then(response => response.json())
+      .then(data => {return data.artists.items[0].id})
+
+    console.log("ArtistID is " + artistID);
+
+    // Get request with Artist ID to grab all the albums from artist
+    var returnedAlbums = await fetch('https://api.spotify.com/v1/artists/' + artistID + '/albums' + '?include_groups=album&market=US&limit=50', searchParameters)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setAlbums(data.items);
+      }); 
+
+    // Display the albums to users
+  }
+  console.log(albums);
+
+  return (
+    <div className="App">
+      <Container>
+        <InputGroup className="mb-3" size="lg">
+          <FormControl
+            placeholder="Search For Artist"
+            type="input"
+            onKeyPress={ event => {
+              if (event.key == "Enter") {
+                search();
+              }
+            }}
+            onChange={event => setSearchInput(event.target.value)}
+          />
+          <Button onClick={search}>
+            Search
+          </Button>
+        </InputGroup>
+      </Container>
+      <Container>
+        <Row className="mx-2 row row-cols-4">
+          {albums.map( (album, i) => {
+            console.log(album);
+            return (
+              <Card>
+                <Card.Img src={album.images[0].url} />
+                <Card.Body>
+                  <Card.Title>{album.name}</Card.Title>
+                </Card.Body>
+              </Card>
+            )
+          })}
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
